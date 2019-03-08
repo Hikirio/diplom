22a20ae0449fab237b21f28b09d0ac6e880e9fcd
anatoliy <anatoliy.matvienkov38@gmail.com>
2019-03-08 16:27:03 +0200

    Initial commit

---
+++ database/migrations/2019_03_08_142051_create_employers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_employers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('company name');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_employers');
+    }
+}
