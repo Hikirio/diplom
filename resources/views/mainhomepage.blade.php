eeaea2b2246a9e31ebb22cf17b407e85ba45d11f
anatoliy <anatoliy.matvienkov38@gmail.com>
2019-03-09 23:11:43 +0200

    navigation and button "toTop"

--- resources/views/mainhomepage.blade.php
+++ resources/views/mainhomepage.blade.php
@@ -61,7 +61,8 @@
         .m-b-md {
             margin-bottom: 30px;
         }
-        .center{
+
+        .center {
             align-items: unset;
             display: flex;
             justify-content: center;
@@ -70,15 +71,63 @@
     </style>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
     <script type="text/javascript">
-        $(document).ready(function(){
-            $("#menu").on("click","a", function (event) {
+        $(document).ready(function () {
+            $("#menu").on("click", "a", function (event) {
                 event.preventDefault();
-                var id  = $(this).attr('href'),
+                var id = $(this).attr('href'),
                     top = $(id).offset().top;
                 $('body,html').animate({scrollTop: top}, 1500);
             });
         });
     </script>
+
+{{--кнопка вверх--}}
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+    <script type="text/javascript">
+
+        $(function () {
+
+            $(window).scroll(function () {
+
+                if ($(this).scrollTop() != 0) {
+
+                    $('#toTop').fadeIn();
+
+                } else {
+
+                    $('#toTop').fadeOut();
+
+                }
+
+            });
+
+            $('#toTop').click(function () {
+
+                $('body,html').animate({scrollTop: 0}, 800);
+
+            });
+
+        });
+
+    </script>
+    <style>
+    #toTop {
+    width:100px;
+    border:1px solid #ccc;
+    background:#f7f7f7;
+    text-align:center;
+    padding:5px;
+    position:fixed;
+    bottom:10px; /* отступ кнопки от нижнего края страницы*/
+    right:10px;
+    cursor:pointer;
+    display:none;
+    color:#333;
+    font-family:verdana;
+    font-size:11px;
+    }
+    </style>
 </head>
 <body>
 
@@ -88,10 +137,10 @@
             @auth
                 <a href="{{ url('/home') }}">Home</a>
             @else
-               <small>Уже зарегестрирован? тебе сюда -></small> <a href="{{ route('login') }}">Вход</a>
+                <small>Уже зарегестрирован? тебе сюда -></small> <a href="{{ route('login') }}">Вход</a>
 
                 {{--@if (Route::has('register'))--}}
-                    {{--<a href="{{ route('register') }}">Register</a>--}}
+                {{--<a href="{{ route('register') }}">Register</a>--}}
                 {{--@endif--}}
             @endauth
         </div>
@@ -116,9 +165,24 @@
 </div>
 
 <div id="yak1" class="position-ref full-height" style="background-color: aquamarine">
-<div class="center" style="color: brown ;"> О нас</div>
+    <div class="center" style="color: brown ;"> О нас</div>
+    <div>
+        "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I
+        will give you a complete account of the system, and expound the actual teachings of the great explorer of the
+        truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is
+        pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are
+        extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because
+        it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great
+        pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain
+        some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has
+        no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+    </div>
 </div>
 
 <div class="flex-center position-ref full-height" id="yak2">1655</div>
+
+
+
+<div ID = "toTop" > ^ Наверх </div>
 </body>
 </html>
